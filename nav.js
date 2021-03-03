f7d3201046a47f84eba6b12c11c04996992b6f18
Akshat Maheshwari <maheshwariakshat482@gmail.com>
2021-03-03 16:36:29 +0530

    Update

--- nav.js
+++ nav.js
@@ -2,8 +2,8 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener('click', ()=>{
-   //Animate Links
+hamburger.addEventListener('click', () => {
+    //Animate Links
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
